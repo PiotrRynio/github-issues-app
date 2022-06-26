d73cd079d61faba2d4bbf4cc7740f5031b447f71
Piotr Rynio <pwrynio@gmail.com>
2022-06-27 01:16:31 +0200

    feat(Typography): add empty styles

--- src/assets/styles/themes/mixins/mixinsValues/typography.ts
+++ src/assets/styles/themes/mixins/mixinsValues/typography.ts
@@ -1,14 +1,42 @@
-import { css, Interpolation, ThemeProps } from 'styled-components';
-import { colors } from '../../themeValues';
-import { fontFamilies } from '../../themeValues';
+import { css, DefaultTheme, Interpolation, ThemeProps } from 'styled-components';
 
 export type Typography = {
-  regular: ReadonlyArray<Interpolation<ThemeProps<any>>>;
+  body1: ReadonlyArray<Interpolation<ThemeProps<DefaultTheme>>>;
+  body2: ReadonlyArray<Interpolation<ThemeProps<DefaultTheme>>>;
+  small: ReadonlyArray<Interpolation<ThemeProps<DefaultTheme>>>;
+  itemTitle: ReadonlyArray<Interpolation<ThemeProps<DefaultTheme>>>;
+  title: ReadonlyArray<Interpolation<ThemeProps<DefaultTheme>>>;
+  subtitle: ReadonlyArray<Interpolation<ThemeProps<DefaultTheme>>>;
+  pagination: ReadonlyArray<Interpolation<ThemeProps<DefaultTheme>>>;
 };
 
 export const typography: Typography = {
-  regular: css`
-    color: ${() => colors.primaryText};
-    font-family: ${() => fontFamilies.primary};
+  body1: css`
+    color: ${({ theme }) => theme.colors.primaryText};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
+  `,
+  body2: css`
+    color: ${({ theme }) => theme.colors.primaryText};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
+  `,
+  small: css`
+    color: ${({ theme }) => theme.colors.primaryText};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
+  `,
+  itemTitle: css`
+    color: ${({ theme }) => theme.colors.primaryText};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
+  `,
+  title: css`
+    color: ${({ theme }) => theme.colors.primaryText};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
+  `,
+  subtitle: css`
+    color: ${({ theme }) => theme.colors.primaryText};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
+  `,
+  pagination: css`
+    color: ${({ theme }) => theme.colors.primaryText};
+    font-family: ${({ theme }) => theme.fontFamilies.primary};
   `,
 };
