8b777b055234e33cce4ec9ef0fa8d45ee18e18ed
Piotr Rynio <pwrynio@gmail.com>
2022-06-26 20:59:41 +0200

    feat(router): add routes in App component

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './views/App/App';
+import { App } from 'views';
 import { AllAppProviders } from './providers';
 
 if (process.env.NODE_ENV === 'development') {
