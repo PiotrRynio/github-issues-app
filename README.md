3034e9edd62f2627905b29628fe97938c4ec0d0e
Piotr Rynio <41823689+PiotrRynio@users.noreply.github.com>
2022-06-26 14:56:09 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# React 17 template + styled-components + useQuery + MSW
+
+### Start:
+
+1. npx npm-check-updates --target minor
+2. npx npm-check-updates -u --target minor
+3. npm install
+4. npm run start
---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './views/App/App';
+
+if (process.env.NODE_ENV === 'development') {
+  const { worker } = require('./mocks/msw/rest-api/browser');
+  worker.start().then();
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
