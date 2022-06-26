a0b9f1837aeb85a270bfb5a3636e4bd673da1b83
Piotr Rynio <pwrynio@gmail.com>
2022-06-26 16:08:00 +0200

    Create logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,3 +1,6 @@
+import logo from 'assets/images/logo/logo.svg';
+import { LogoImage } from './Logo.styles';
+
 export const Logo = () => {
-  return <div>logo</div>;
+  return <LogoImage src={logo} alt={'github logo'} />;
 };
