4850d43c95a3b6d894f47519139b4c36cd3b53a3
Piotr Rynio <pwrynio@gmail.com>
2022-06-28 18:32:48 +0200

    feat(User): add User view styles

---
+++ src/views/User/User.styles.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.article`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    margin-top: 15px;
+  }
+`;
+
+export const Avatar = styled.img`
+  display: block;
+  width: 296px;
+  height: 296px;
+  border-radius: 50%;
+`;
+
+export const TitlesContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 16px auto 13px;
+`;
+
+export const StatsContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 19px;
+`;
--- src/views/User/User.tsx
+++ src/views/User/User.tsx
@@ -1,22 +1,32 @@
-import { useGithubUser } from '../../hooks/useUser';
-import { Typography } from '../../components';
+import { useGithubUser } from 'hooks/useUser';
+import { Typography } from 'components';
+import { Avatar, StatsContainer, TitlesContainer, Wrapper } from './User.styles';
 
 export const User = () => {
   const { data } = useGithubUser('defunkt');
 
   if (!data) {
-    return <div>Loading...</div>;
+    return (
+      <Wrapper>
+        <Typography variant="body1"> Loading...</Typography>
+      </Wrapper>
+    );
   }
+
   const { avatar, login, name, followersNumber, followingNumber, starsNumber } = data;
 
   return (
-    <article>
-      <img src={avatar} alt={`avatar of ${name} user`} />
-      <Typography variant="title">{name}</Typography>
-      <Typography variant="subtitle">{login}</Typography>
-      <Typography variant="small">{followersNumber}</Typography>
-      <Typography variant="small">{followingNumber}</Typography>
-      <Typography variant="small">{starsNumber}</Typography>
-    </article>
+    <Wrapper>
+      <Avatar src={avatar} alt={`avatar of ${name} user`} />
+      <TitlesContainer>
+        <Typography variant="title">{name}</Typography>
+        <Typography variant="subtitle">{login}</Typography>{' '}
+      </TitlesContainer>
+      <StatsContainer>
+        <Typography variant="small">{followersNumber}</Typography>
+        <Typography variant="small">{followingNumber}</Typography>
+        <Typography variant="small">{starsNumber}</Typography>
+      </StatsContainer>
+    </Wrapper>
   );
 };
