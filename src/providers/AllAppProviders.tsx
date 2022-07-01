535cfc89b153b3c295c09e85a179c954e8aa4ee2
Piotr Rynio <pwrynio@gmail.com>
2022-07-02 00:59:23 +0200

    fix(helmet): fix helmet warning

--- src/providers/AllAppProviders.tsx
+++ src/providers/AllAppProviders.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
-import { Helmet } from 'react-helmet';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { GlobalStyles, theme } from 'assets/styles/themes/primaryTheme';
 import { usePageTitle } from 'hooks/usePageTitle/usePageTitle';
 import { PageTitleProvider } from './appProviders';
@@ -12,18 +12,20 @@ export const AllAppProviders = ({ children }: { children: ReactNode }) => {
   const { pageTitle } = usePageTitle();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <PageTitleProvider>
-        <ThemeProvider theme={theme}>
-          <Helmet>
-            <title>{pageTitle}</title>
-            <meta name="description" content="Github searcher application" />
-            <link rel="stylesheet" href={fontsPaths.primaryFont} />
-          </Helmet>
-          <GlobalStyles theme={theme} />
-          {children}
-        </ThemeProvider>
-      </PageTitleProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <PageTitleProvider>
+          <ThemeProvider theme={theme}>
+            <Helmet>
+              <title>{pageTitle}</title>
+              <meta name="description" content="Github searcher application" />
+              <link rel="stylesheet" href={fontsPaths.primaryFont} />
+            </Helmet>
+            <GlobalStyles theme={theme} />
+            {children}
+          </ThemeProvider>
+        </PageTitleProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 };
