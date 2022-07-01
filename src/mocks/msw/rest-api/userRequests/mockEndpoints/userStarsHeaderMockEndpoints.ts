9edf477a76c5f12e0ec15eb1a7ec58445e724151
Piotr Rynio <pwrynio@gmail.com>
2022-07-01 23:30:31 +0200

    feat(project): fix tests warning

--- src/mocks/msw/rest-api/userRequests/mockEndpoints/userStarsHeaderMockEndpoints.ts
+++ src/mocks/msw/rest-api/userRequests/mockEndpoints/userStarsHeaderMockEndpoints.ts
@@ -4,7 +4,7 @@ import { server } from 'mocks/msw/rest-api/server';
 
 export const getUserStarsHeaderWillReturn = ({ starsNumber }: { starsNumber: number }) => {
   server.use(
-    rest.get(`${REST_API_URL}${USERS_PATH}/:githubUserLogin/starred?per_page=1`, (req, res, ctx) => {
+    rest.get(`${REST_API_URL}${USERS_PATH}/:githubUserLogin/starred`, (req, res, ctx) => {
       const link = `<https://api.github.com/user/2/starred?per_page=1&page=2>; rel="next", <https://api.github.com/user/2/starred?per_page=1&page=${starsNumber}>; rel="last"`;
       return res(ctx.status(200), ctx.set('Link', link));
     }),
