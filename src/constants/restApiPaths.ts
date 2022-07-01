c422e3754b6eb622c2d9a7304f7568f8eb83e51e
Piotr Rynio <pwrynio@gmail.com>
2022-07-01 20:36:32 +0200

    fix(paths): fix paths

--- src/constants/restApiPaths.ts
+++ src/constants/restApiPaths.ts
@@ -1,3 +1,5 @@
 export const REST_API_URL = 'https://api.github.com';
 
 export const USERS_PATH = '/users';
+export const USER_SEARCHER_PATH = '/search/users';
+export const REPOSITORIES_SEARCHER_PATH = '/search/repositories';
